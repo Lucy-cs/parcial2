a839402901781c4621902d2698b7b10aa9267d58
Daniel Valenzuela <hetzondaniel46@gmail.com>
2024-09-28 22:54:01 -0600

    se agregaron 2 funcionalidades

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const contactButtons = document.querySelectorAll('.boton');
 
-    // Añade el evento click a cada botón
+   
     contactButtons.forEach(function(button) {
         button.addEventListener('click', function() {
             alert('Llámanos al +502 7854-9658 y te atenderemos con gusto');
         });
     });
+});
+
+window.addEventListener('load', function() {
+    alert('Se recargó la página');
 });
